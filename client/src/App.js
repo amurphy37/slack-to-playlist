9acbe8dca51831d2886ec4beb46220d4248e5d9d
amurphy37 <amurphy37@gmail.com>
2023-12-21 00:32:57 -0800

    updating structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Layout, Header, Navigation, Content } from 'react-mdl';
-import Main from './components/main';
-import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="AppParentDiv">
-      <Layout>
-        <Header className="header-color" title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">SlackToPlaylist</Link>} scroll>
-          <Navigation>
-            <Link to="/register">Register Today</Link>
-            <Link to="/login">Log In</Link>
-          </Navigation>
-        </Header>
-        <Content>
-          <div className="page-content" />
-          <Main />
-        </Content>
-      </Layout>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
